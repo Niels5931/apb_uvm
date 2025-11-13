490eb68fcf5adbe3367f1dd8c622fa78cb8fda92
Niels Hartmann <nwhartmann@gmail.com>
2025-11-13 20:26:13 +0100

    Added seq_lib

---
+++ src/uvc/cl_apb_seq_lib.svh
@@ -0,0 +1,50 @@
+
+class cl_apb_base_seq extends uvm_sequence#(cl_apb_seq_item);
+
+  cl_apb_seq_item s_item;
+
+  `uvm_object_utils(cl_apb_base_seq)
+
+
+  function new(string name = "cl_apb_base_seq");
+    super.new(name);
+  endfunction 
+
+  virtual task body();
+    `uvm_fatal("cl_apb_base_seq","Seq must override task body")
+  endtask : body
+
+endclass : cl_apb_base_seq
+
+class cl_apb_single_seq extends cl_apb_base_seq;
+  
+  `uvm_object_utils(cl_apb_single_seq)
+
+  function new(string name = "cl_apb_single_seq");
+    super.new(name);
+  endfunction
+
+  virtual task body();
+    // the seq item is created and randomized if not set during simulation
+    start_item(s_item);
+    if (!s_item.randomize()) begin
+      `uvm_fatal("cl_apb_single_seq","Failed to randomize seq item!")
+    end
+
+    finish_item(s_item);
+    get_response(s_item);
+
+  endtask : body
+
+  function void pre_randomize();
+    pre_randomize();
+    if (s_item == null) begin
+      s_item = cl_apb_seq_item::type_id::create();
+      if (!s_item.randomize()) begin
+        `uvm_fatal("cl_apb_single_seq","Failed to randomize seq item!")
+      end
+    end
+
+  endfunction : pre_randomize
+
+endclass : cl_apb_single_seq
--- src/uvc/pk_apb.sv
+++ src/uvc/pk_apb.sv
@@ -4,7 +4,10 @@ package pk_apb;
   `include "uvm_macros.svh"
   `include "apb_common.svh"
   `include "cl_apb_seq_item.svh"
-  `include "cl_apb_driver_base.svh"
+  `include "cl_apb_seq_lib.svh"
   `include "cl_apb_config.svh"
+  `include "cl_apb_driver_base.svh"
+  `include "cl_apb_driver_manager.svh"
+  `include "cl_apb_agent.svh"
 
 endpackage : pk_apb
